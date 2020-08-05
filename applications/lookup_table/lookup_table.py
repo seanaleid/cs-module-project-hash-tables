21b1038b8934df497a1962eac391d58c807cc172
seanaleid <seanaleid@gmail.com>
2020-08-05 13:32:32 -0400

    Complete lookup_table.py

--- applications/lookup_table/lookup_table.py
+++ applications/lookup_table/lookup_table.py
@@ -1,5 +1,7 @@
 # Your code here
 
+import random
+import math
 
 def slowfun_too_slow(x, y):
     v = math.pow(x, y)
@@ -9,14 +11,31 @@ def slowfun_too_slow(x, y):
 
     return v
 
+cache = {}
+
 def slowfun(x, y):
     """
     Rewrite slowfun_too_slow() in here so that the program produces the same
     output, but completes quickly instead of taking ages to run.
     """
     # Your code here
+    """ second attempt """
+    if (x,y) not in cache:
+        cache[(x,y)] = slowfun_too_slow(x,y)
+    
+    return cache[(x,y)]
 
 
+    """ first pass """
+    # if x and y in cache:
+    #     return cache[x,y]
+    # else:
+    #     v = math.pow(x, y)
+    #     v = math.factorial(v)
+    #     v //= (x + y)
+    #     v %= 982451653
+    #     cache[x,y] = v
+    #     return v
 
 # Do not modify below this line!
 
